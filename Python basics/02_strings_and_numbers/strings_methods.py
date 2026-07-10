b7fc7d804133f288ce4b6b223d21a851ef4b4a6f
harshini45678 <birojharshini@gmail.com>
2026-07-10 21:25:38 +0530

    Initial commit
    Added programs demonstrating user input using input() and a basic calculator using arithmetic operators.

---
+++ Python basics/02_strings_and_numbers/strings_methods.py
@@ -0,0 +1,11 @@
+name = "harshu"
+
+print(name)
+print(name.capitalize())
+print(name.isalpha())
+print(name.islower())
+print(name.upper())
+print(name.lower())
+print(len(name))
+print(name[3])
+print(name[5])
