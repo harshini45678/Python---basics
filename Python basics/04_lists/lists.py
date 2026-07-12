ebedbba7e47d242a7d413c24ad03eac5ccc1ff7e
harshini45678 <birojharshini@gmail.com>
2026-07-12 13:17:07 +0530

    Add files via upload

---
+++ Python basics/04_lists/lists.py
@@ -0,0 +1,9 @@
+fav_numbers = [2, 8, 20, 4, 6, 10]
+fav_colors = ["pink", "blue", "black", "yellow", "orange"]
+print(fav_numbers)
+print(fav_colors)
+print(fav_numbers[0:3])
+print(fav_colors[1])
+print(fav_numbers[-1])
+print(fav_colors[-2])
+print(fav_colors[1:4])
